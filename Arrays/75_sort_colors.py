f67c0cb16c2d0b71ccaf8b7c2d77ec9e173a2cdd
Nikita Kunevich <procion95@gmail.com>
2020-06-24 10:26:39 +0300

    added new solution to sort_colors

--- Arrays/75_sort_colors.py
+++ Arrays/75_sort_colors.py
@@ -1,4 +1,6 @@
 class Solution:
+    # O(n) time | O(1) space
+    # user pointers to track last non-zero item, first non-two item, current item
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -16,3 +18,22 @@ class Solution:
                 right -= 1
             else: cur += 1
         return nums
+
+    # O(N) time | O(1) space
+    # Use counting sort
+    def sortColorsCountingSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # count sort
+        amounts = [0,0,0]
+        for n in nums:
+            amounts[n] += 1
+        cur = 0
+        for i in range(len(nums)):
+            while amounts[cur] == 0:
+                cur +=1
+                if cur > 2:
+                    return [nums]
+            nums[i] = cur
+            amounts[cur] -= 1
